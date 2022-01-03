29fda9d39c14c737e76df02a0241afc899b27ce4
David Leach <davidleach724@gmail.com>
2022-01-03 13:21:24 -0700

    Create functional components for nav, footer, and home

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,14 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../Home/Home';
 import './App.css';
 
 const App = () => {
 
   return (
     <main>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={ Home } />
+      </Routes>
     </main>
   )
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import './NavBar.css'
+
+const NavBar = () => {
+
+  return (
+    <nav>
+      <h1>This is the nav component</h1>
+    </nav>
+  )
+}
+
+export default NavBar
