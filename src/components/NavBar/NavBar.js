a60183b0a159ee6e117f95f7373b04b4b64ac112
David Leach <davidleach724@gmail.com>
2022-01-03 15:44:49 -0700

    Create dropdown menu

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,44 @@
 import './NavBar.css'
 import quizMeLogo from '../../images/logo_quizme.png'
 import menuButton from '../../images/menu.png'
+import { useEffect, useRef, useState } from 'react'
 
 const NavBar = () => {
+  const dropdownRef = useRef(null);
+  const [isActive, setIsActive] = useState(false);
+  const onClick = () => setIsActive(!isActive);
+
+  useEffect(() => {
+    const pageClickEvent = (e) => {
+      if (dropdownRef.current !== null && !dropdownRef.current.contains(e.target)) {
+        setIsActive(!isActive);
+      }
+    }
+
+    if (isActive) {
+      window.addEventListener('click', pageClickEvent);
+    }
+
+    return() => {
+      window.removeEventListener('click', pageClickEvent)
+    }
+  }, [isActive])
 
   return (
-    <nav>
-      <img src={ menuButton } className='menu-button' alt='menu button'></img>
+    <header>
+      <div className='menu-container'>
+        <button onClick={onClick} className='menu-trigger'>
+          <img src={ menuButton } className='menu-button' alt='menu button'></img>
+        </button>
+        <nav ref={dropdownRef} className={`menu ${isActive ? 'active' : 'inactive'}`}>
+          <ul>
+            <li><a href='/Instructions'>Instructions</a></li>
+            <li><a href='Scoreboard'>Scoreboard</a></li>
+          </ul>
+        </nav>
+      </div>
       <img src = { quizMeLogo } className='quiz-me-logo' alt='quiz me logo'></img>
-    </nav>
+    </header>
   )
 }
 
