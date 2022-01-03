94d2a64e4351bc3e43f3f4604e2e389a9e28e28f
David Leach <davidleach724@gmail.com>
2022-01-03 13:28:35 -0700

    Upgrade router to v6

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 import Home from '../Home/Home';
+import NavBar from '../NavBar/NavBar';
 import './App.css';
 
 const App = () => {
 
   return (
     <main>
+      <NavBar />
       <Routes>
-        <Route exact path="/" element={ Home } />
+        <Route exact path="/" element={ <Home /> } />
       </Routes>
+      <Footer />
     </main>
   )
 }
