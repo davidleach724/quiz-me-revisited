b9c4e0539e6e43a60197cc05b812e17163a9bf58
David Leach <davidleach724@gmail.com>
2022-01-04 10:29:38 -0700

    Add buttons to center

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,23 @@
 import './Home.css'
+import easyBtn from '../../images/easy_btn.png'
+import mediumBtn from '../../images/medium_btn.png'
+import hardBtn from '../../images/hard_btn.png'
 
 const Home = () => {
 
   return (
-    <div>
-      <h1>This is home component</h1>
+    <div className='home-container'>
+      <div className='category-container'>
+        <h3>Select A Category</h3>
+      </div>
+      <div className='button-container'>
+        <img src={ easyBtn } className='difficulty-button'></img>
+        <img src={ mediumBtn } className='difficulty-button'></img>
+        <img src={ hardBtn } className='difficulty-button'></img>
+      </div>
+      <div className='lets-play-container'>
+        <h3>YOU HAVE SELECTED _____ LEVEL ______</h3>
+      </div>
     </div>
   )
 }
