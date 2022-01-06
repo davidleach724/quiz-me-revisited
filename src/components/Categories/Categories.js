11550336df748d7b06051e8533da053b2ec9d7b4
David Leach <davidleach724@gmail.com>
2022-01-05 17:05:21 -0700

    Formats categories and home

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,14 +1,17 @@
 const Categories = ({ categoryData, handleCategoryChange }) => {
-  
   const categoryList = categoryData.map((category) => {
     return (
-      <option value={category.name} key={category.id}>{category.name}</option>
-    )
-  })
+      <option value={category.name} key={category.id}>
+        {category.name}
+      </option>
+    );
+  });
 
   return (
-    <select onChange={e => handleCategoryChange(e.target.value)}>{ categoryList }</select>
-  )
-}
+    <select onChange={(e) => handleCategoryChange(e.target.value)}>
+      {categoryList}
+    </select>
+  );
+};
 
-export default Categories
+export default Categories;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,47 +1,64 @@
-import './Home.css'
-import easyBtn from '../../images/easy_btn.png'
-import mediumBtn from '../../images/medium_btn.png'
-import hardBtn from '../../images/hard_btn.png'
-import { useState } from 'react/cjs/react.development'
-import { useEffect } from 'react'
-import { fetchCategories } from '../../api calls/apicalls'
-import Categories from '../Categories/Categories'
+import './Home.css';
+import easyBtn from '../../images/easy_btn.png';
+import mediumBtn from '../../images/medium_btn.png';
+import hardBtn from '../../images/hard_btn.png';
+import { useState } from 'react/cjs/react.development';
+import { useEffect } from 'react';
+import { fetchCategories } from '../../api calls/apicalls';
+import Categories from '../Categories/Categories';
 
 const Home = () => {
-  const [difficulty, setDifficulty] = useState(null)
-  const [categories, setCategories] = useState(null)
-  const [selectedCategory, setSelectedCategory] = useState(null)
+  const [difficulty, setDifficulty] = useState(null);
+  const [categories, setCategories] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
   useEffect(() => {
     if (categories === null) {
-      fetchCategories()
-      .then(data => setCategories(data.trivia_categories))
+      fetchCategories().then((data) => setCategories(data.trivia_categories));
     }
-  })
+  });
 
   const handleCategoryChange = (category) => {
-    return setSelectedCategory(category)
-  }
+    return setSelectedCategory(category);
+  };
 
-  
   return (
-    <div className='home-container'>
-      <div className='category-container'>
+    <div className="home-container">
+      <div className="category-container">
         <h3>Select A Category</h3>
-        {categories && <Categories categoryData={ categories } handleCategoryChange={handleCategoryChange}/>}
+        {categories && (
+          <Categories
+            categoryData={categories}
+            handleCategoryChange={handleCategoryChange}
+          />
+        )}
       </div>
-      <div className='button-container'>
-        <img src={ easyBtn } className='difficulty-button' onClick={() => setDifficulty('EASY')}></img>
-        <img src={ mediumBtn } className='difficulty-button' onClick={() => setDifficulty('MEDIUM')}></img>
-        <img src={ hardBtn } className='difficulty-button' onClick={() => setDifficulty('HARD')}></img>
+      <div className="button-container">
+        <img
+          src={easyBtn}
+          className="difficulty-button"
+          onClick={() => setDifficulty('EASY')}
+        ></img>
+        <img
+          src={mediumBtn}
+          className="difficulty-button"
+          onClick={() => setDifficulty('MEDIUM')}
+        ></img>
+        <img
+          src={hardBtn}
+          className="difficulty-button"
+          onClick={() => setDifficulty('HARD')}
+        ></img>
       </div>
-      <div className='lets-play-container'>
-        {(difficulty && selectedCategory) && 
-        <h3>YOU SLECTED {selectedCategory.toUpperCase()} LEVEL {difficulty}</h3>
-        }
+      <div className="lets-play-container">
+        {difficulty && selectedCategory && (
+          <h3>
+            YOU SLECTED {selectedCategory.toUpperCase()} LEVEL {difficulty}
+          </h3>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
